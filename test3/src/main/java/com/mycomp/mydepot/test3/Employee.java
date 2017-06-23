8946bb14cc6333327ff921de867870c685877e7e
samkk2 <samzchicago@gmail.com>
2017-06-23 07:01:05 +0400

    my test3 project in the correct repo

---
+++ test3/src/main/java/com/mycomp/mydepot/test3/Employee.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycomp.mydepot.test3;
+
+/**
+ *
+ * @author Winuser
+ */
+public class Employee {
+ private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "name=" + name + '}';
+    }
+ 
+}
